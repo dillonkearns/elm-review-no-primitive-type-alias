44b1ad4de900229a2277b0d242f7fa91e031a449
Dillon Kearns <dillon@incrementalelm.com>
2022-02-28 09:43:44 -0800

    Inject module lookup table.

--- src/NoPrimitiveTypeAlias.elm
+++ src/NoPrimitiveTypeAlias.elm
@@ -9,6 +9,7 @@ module NoPrimitiveTypeAlias exposing (rule)
 import Elm.Syntax.Declaration as Declaration exposing (Declaration)
 import Elm.Syntax.Node as Node exposing (Node)
 import Elm.Syntax.TypeAnnotation as TypeAnnotation exposing (TypeAnnotation)
+import Review.ModuleNameLookupTable as ModuleNameLookupTable exposing (ModuleNameLookupTable)
 import Review.Rule as Rule exposing (Error, Rule)
 
 
@@ -48,35 +49,48 @@ elm-review --template dillonkearns/elm-review-no-primitive-type-alias/example --
 -}
 rule : Rule
 rule =
-    Rule.newModuleRuleSchema "NoPrimitiveTypeAlias" ()
-        |> Rule.withSimpleDeclarationVisitor declarationVisitor
+    Rule.newModuleRuleSchemaUsingContextCreator "NoPrimitiveTypeAlias" initialContext
+        |> Rule.withDeclarationEnterVisitor declarationVisitor
         |> Rule.fromModuleRuleSchema
 
 
-declarationVisitor : Node Declaration -> List (Error {})
-declarationVisitor node =
+type alias Context =
+    { lookupTable : ModuleNameLookupTable }
+
+
+initialContext : Rule.ContextCreator () Context
+initialContext =
+    Rule.initContextCreator
+        (\lookupTable () -> { lookupTable = lookupTable })
+        |> Rule.withModuleNameLookupTable
+
+
+declarationVisitor : Node Declaration -> Context -> ( List (Error {}), Context )
+declarationVisitor node context =
     case Node.value node of
         Declaration.AliasDeclaration aliasDeclaration ->
-            if isPrimitive (Node.value aliasDeclaration.typeAnnotation) then
-                [ Rule.error
-                    { message = "Primitive type alias for `" ++ Node.value aliasDeclaration.name ++ "`"
-                    , details =
-                        [ "Type aliases to simple primitives like String or Int can be misleading because you could give them a name, like `type alias UserId = String`, but then use that name anywhere a String is used. That means you could have a home address that is mistakenly labeled as a `UserId`, or an actual user ID string that is labelled as `String`."
-                        , "Consider using a custom type, like `type UserId = UserId String` instead so the compiler can give you more useful feedback."
-                        ]
-                    }
-                    (Node.range node)
-                ]
+            if isPrimitive context.lookupTable (Node.value aliasDeclaration.typeAnnotation) then
+                ( [ Rule.error
+                        { message = "Primitive type alias for `" ++ Node.value aliasDeclaration.name ++ "`"
+                        , details =
+                            [ "Type aliases to simple primitives like String or Int can be misleading because you could give them a name, like `type alias UserId = String`, but then use that name anywhere a String is used. That means you could have a home address that is mistakenly labeled as a `UserId`, or an actual user ID string that is labelled as `String`."
+                            , "Consider using a custom type, like `type UserId = UserId String` instead so the compiler can give you more useful feedback."
+                            ]
+                        }
+                        (Node.range node)
+                  ]
+                , context
+                )
 
             else
-                []
+                ( [], context )
 
         _ ->
-            []
+            ( [], context )
 
 
-isPrimitive : TypeAnnotation -> Bool
-isPrimitive annotation =
+isPrimitive : ModuleNameLookupTable -> TypeAnnotation -> Bool
+isPrimitive lookupTable annotation =
     case annotation of
         TypeAnnotation.Unit ->
             True
