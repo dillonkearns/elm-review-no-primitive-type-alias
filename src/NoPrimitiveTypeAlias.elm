07c98575d0ad5a8724e9e0b593e79b2a74ef5ed7
Dillon Kearns <dillon@incrementalelm.com>
2022-02-28 09:34:31 -0800

    Extract isPrimitive helper.

--- src/NoPrimitiveTypeAlias.elm
+++ src/NoPrimitiveTypeAlias.elm
@@ -8,7 +8,7 @@ module NoPrimitiveTypeAlias exposing (rule)
 
 import Elm.Syntax.Declaration as Declaration exposing (Declaration)
 import Elm.Syntax.Node as Node exposing (Node)
-import Elm.Syntax.TypeAnnotation as TypeAnnotation
+import Elm.Syntax.TypeAnnotation as TypeAnnotation exposing (TypeAnnotation)
 import Review.Rule as Rule exposing (Error, Rule)
 
 
@@ -57,40 +57,46 @@ declarationVisitor : Node Declaration -> List (Error {})
 declarationVisitor node =
     case Node.value node of
         Declaration.AliasDeclaration aliasDeclaration ->
-            let
-                errorMessage =
-                    Rule.error
-                        { message = "Primitive type alias for `" ++ Node.value aliasDeclaration.name ++ "`"
-                        , details =
-                            [ "Type aliases to simple primitives like String or Int can be misleading because you could give them a name, like `type alias UserId = String`, but then use that name anywhere a String is used. That means you could have a home address that is mistakenly labeled as a `UserId`, or an actual user ID string that is labelled as `String`."
-                            , "Consider using a custom type, like `type UserId = UserId String` instead so the compiler can give you more useful feedback."
-                            ]
-                        }
-                        (Node.range node)
-            in
-            case Node.value aliasDeclaration.typeAnnotation of
-                TypeAnnotation.Unit ->
-                    [ errorMessage ]
+            if isPrimitive (Node.value aliasDeclaration.typeAnnotation) then
+                [ Rule.error
+                    { message = "Primitive type alias for `" ++ Node.value aliasDeclaration.name ++ "`"
+                    , details =
+                        [ "Type aliases to simple primitives like String or Int can be misleading because you could give them a name, like `type alias UserId = String`, but then use that name anywhere a String is used. That means you could have a home address that is mistakenly labeled as a `UserId`, or an actual user ID string that is labelled as `String`."
+                        , "Consider using a custom type, like `type UserId = UserId String` instead so the compiler can give you more useful feedback."
+                        ]
+                    }
+                    (Node.range node)
+                ]
 
-                TypeAnnotation.Typed a b ->
-                    case Node.value a of
-                        ( [], "String" ) ->
-                            [ errorMessage ]
-
-                        ( [], "Int" ) ->
-                            [ errorMessage ]
-
-                        ( [], "Bool" ) ->
-                            [ errorMessage ]
-
-                        ( [], "Float" ) ->
-                            [ errorMessage ]
-
-                        _ ->
-                            []
-
-                _ ->
-                    []
+            else
+                []
 
         _ ->
             []
+
+
+isPrimitive : TypeAnnotation -> Bool
+isPrimitive annotation =
+    case annotation of
+        TypeAnnotation.Unit ->
+            True
+
+        TypeAnnotation.Typed a b ->
+            case Node.value a of
+                ( [], "String" ) ->
+                    True
+
+                ( [], "Int" ) ->
+                    True
+
+                ( [], "Bool" ) ->
+                    True
+
+                ( [], "Float" ) ->
+                    True
+
+                _ ->
+                    False
+
+        _ ->
+            False
